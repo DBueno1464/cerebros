223394a489ef449ce1e14577380c9c3aa6500271
Stephen Wong <yummywongttv@gmail.com>
2021-11-13 12:51:29 -0500

    changed react

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -10,3 +11,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+=======
+import App from './components/App';
+
+ReactDOM.render( < App / > , document.getElementById('root'));
+>>>>>>> Stashed changes
