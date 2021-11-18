66a8b2a8914993f490680e14470846610e1c28ff
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-18 16:55:35 -0500

    updated path to queries in thoughtform

--- client/src/components/thoughts/ThoughtForm.js
+++ client/src/components/thoughts/ThoughtForm.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 
-import { ADD_THOUGHT } from "../../../src/utils/mutations";
-import { QUERY_THOUGHTS, GET_ME } from "../../../src/utils/queries";
+import { ADD_THOUGHT } from "../../utils/mutations";
+import { QUERY_THOUGHTS, GET_ME } from "../../utils/queries";
 
 import Auth from "../../utils/auth";
 
