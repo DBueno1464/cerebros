7c613f12cebf3565f3459eae2608111fd001aeba
Stephen Wong <yummywongttv@gmail.com>
2021-11-19 09:43:38 -0500

    update/create
    updated app.js/ created styles, made new .js files (add fav, remove fav, movie list headings, movie lists, search box)

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,57 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
-// import  logo from './logo.svg';
-import Navbar from './components/Navbar';
-import SavedContent from './pages/SavedContent';
-import SearchContent from './pages/SearchContent';
-import Search from './components/Search';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
 
-import env from 'react-dotenv';
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+	const [favourites, setFavourites] = useState([]);
 
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-const httpLink = new createHttpLink({
-  uri: "/graphql",
-});
+		const response = await fetch(url);
+		const responseJson = await response.json();
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+	};
 
-// console.log(`${process.env.REACT_APP_URL}${process.env.REACT_APP_APIKEY}`);
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
-function App() {
-  return (
-    <Search />
-  );
-}
-
-// function App() {
-//   return (
-//     <ApolloProvider client={client}>
-//       <Router>
-//         <>
-//           <Navbar />
-//           <Switch>
-//             <Route exact path="/" component={SearchContent} />
-//             <Route exact path="/saved" component={SavedContent} />
-//             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-//           </Switch>
-//         </>
-//       </Router>
-//     </ApolloProvider>
-//   );
-// }
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					favouriteComponent={AddFavourites}
+					handleFavouritesClick={addFavouriteMovie}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList movies={favourites} favouriteComponent={AddFavourites} />
+			</div>
+		</div>
+	);
+};
 
 export default App;
