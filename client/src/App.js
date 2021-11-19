a993630273f9629dcb7b8d8cfc5e310f3eecc38f
Stephen Wong <yummywongttv@gmail.com>
2021-11-19 13:06:11 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,8 @@ import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddToFavourites';
 import RemoveFavourites from './components/RemoveFavourites';
-import Navbar from './components/Navbar';
 
 const App = () => {
-	const [navbar, setNavbar] = useState ([])
 	const [movies, setMovies] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
 	const [favourites, setFavourites] = useState([]);
@@ -74,57 +72,56 @@ const App = () => {
 
 export default App;
 
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+// import { setContext } from '@apollo/client/link/context';
 
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
-import LandingPage from './pages/LandingPage';
-import SavedContent from './pages/SavedContent';
-import SearchContent from './pages/SearchContent';
-
-
-const httpLink = new createHttpLink({
-  uri: "/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+// import LandingPage from './pages/LandingPage';
+// import SavedContent from './pages/SavedContent';
+// import SearchContent from './pages/SearchContent';
 
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Navbar />
-          <div className="container">
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/search" component={SearchContent} />
-            <Route exact path="/saved" component={SavedContent} />
-            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
-          </div>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
-}
+// const httpLink = new createHttpLink({
+//   uri: "/graphql",
+// });
 
-export default App;
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+
+// function App() {
+//   return (
+//     <ApolloProvider client={client}>
+//       <Router>
+//         <div className="flex-column justify-flex-start min-100-vh">
+//           <Navbar />
+//           <div className="container">
+//           <Switch>
+//             <Route exact path="/" component={LandingPage} />
+//             <Route exact path="/search" component={SearchContent} />
+//             <Route exact path="/saved" component={SavedContent} />
+//             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+//           </Switch>
+//           </div>
+//         </div>
+//       </Router>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
 
