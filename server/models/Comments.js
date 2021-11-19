0dcfebd9d362f3cbac7556f631bf8912eb3f06b5
Roberto Monplaisir <castedcrown84@gmail.com>
2021-11-19 13:57:53 -0500

    completed comments schema

--- server/models/Comments.js
+++ server/models/Comments.js
@@ -1,4 +1,12 @@
 const { Schema, model } = require("mongoose");
 
 
-const commentSchema = new Schema
+const commentSchema = new Schema( {
+    comment: { type: String, required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post"},
+    user: { type: Schema.Types.ObjectId, ref: "User"},
+    upvotes: { type: Number },
+    downvotes: { type: Number }
+  },
+  { timestamps: true }
+)
